a33f3d2f93302525e3d30691b6dac7d8c4ac9f09
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-01-27 16:07:34 +0700

    feat: add null state and loading state

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { Plus } from 'lucide-react';
 import React from 'react';
+import { ImSpinner8 } from 'react-icons/im';
 
-import Button from '@/components/buttons/Button';
 import Layout from '@/components/layout/Layout';
+import ButtonLink from '@/components/links/ButtonLink';
 import Seo from '@/components/Seo';
 
 import Table from '@/pages/products/components/Table';
@@ -14,10 +15,12 @@ import { ProductType } from '@/types/products';
 
 export default function ProductList() {
   const [productsData, setProductsData] = React.useState<ProductType[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   React.useEffect(() => {
     axios
       .get<ApiReturn<ProductType[]>>('/api/product')
-      .then((res) => setProductsData(res.data.data));
+      .then((res) => setProductsData(res.data.data))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
@@ -27,13 +30,29 @@ export default function ProductList() {
         <section className='layout relative flex flex-col gap-4'>
           <h1>List Products</h1>
           <div className='flex flex-row justify-end'>
-            <Button variant='primary' className='mr-2' leftIcon={Plus}>
+            <ButtonLink
+              href='/products/create'
+              variant='primary'
+              className='mr-2'
+              leftIcon={Plus}
+            >
               Add Product
-            </Button>
+            </ButtonLink>
           </div>
         </section>
         <section className='layout relative'>
-          <Table title={TABLE_HEAD_TITLE} data={productsData} />
+          {isLoading ? (
+            <div className='flex flex-col items-center justify-center py-4 text-gray-800'>
+              <ImSpinner8 className='mb-4 animate-spin text-4xl' />
+              <p>Loading...</p>
+            </div>
+          ) : productsData.length > 0 ? (
+            <Table title={TABLE_HEAD_TITLE} data={productsData} />
+          ) : (
+            <div className='flex flex-col items-center justify-center py-4 text-gray-800'>
+              <p>No data found</p>
+            </div>
+          )}
         </section>
       </main>
     </Layout>
